eed183146bc2121e4c1bf172316c586d35bcf7be
Kritebh Lagan Bibhakar <54534607+kritebh@users.noreply.github.com>
2022-12-15 08:56:50 +0000

    added server to test

--- Sending Dynamic Email/email.js
+++ Sending Dynamic Email/email.js
@@ -1,13 +1,16 @@
+const express = require("express");
+const app = express();
+
 const emailTemplate = require("./template").email
 
 // let finalHTMLstring = emailTemplate.replace("{{name}}","Cataloguer")
 // finalHTMLstring = finalHTMLstring.replace("{{otp}}",6427)
 // console.log(finalHTMLstring)
 
-let mapObj = {
-    "{{name}}":"John",
-    "{{otp}}":6754
-}
+// let mapObj = {
+//     "{{name}}":"John",
+//     "{{otp}}":6754
+// }
 
 
 
@@ -19,4 +22,23 @@ function replaceAll(str,mapObj){
     });
 }
 
-console.log(replaceAll(emailTemplate,mapObj))
+
+// /send?name=john
+app.get("/send",(req,res)=>{
+    console.log("Query => ",req.query.name)
+    // let {name} = req.query.name
+
+    let mapObj = {
+        "{{name}}":req.query.name,
+        "{{otp}}":Math.floor(1000+Math.random()*9000)
+    }
+
+    let emailToBeSend = replaceAll(emailTemplate,mapObj);
+
+    res.send(emailToBeSend)
+
+})
+
+app.listen(5000,()=>{
+    console.log("Server is running on 5000")
+})
