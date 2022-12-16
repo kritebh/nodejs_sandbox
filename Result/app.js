b8db6eaa97d63807aad184431e9de4a7ce8fe0b2
Kritebh Lagan Bibhakar <54534607+kritebh@users.noreply.github.com>
2022-12-16 03:29:43 +0000

    result scraped

---
+++ Result/app.js
@@ -0,0 +1,33 @@
+const axios = require("axios")
+const fs = require("fs")
+
+async function fetchResult(roll){
+    let response = await axios.get(`https://csvtu.digivarsity.online/WebApp/Result/SemesterResult.aspx?S=6+SEMESTER&T=Regular&E=Apr-May+2022&R=${roll}`)
+    let html = response.data;
+    try{
+        let splittedHtml = html.split(`<span id="obtmarks">`)
+        let marksInSpan = splittedHtml[1]
+        let marks = marksInSpan.split("</span>")[0]
+
+        let nameSplittedHtml = html.split(`<span id="NOC" style="line-height: 28px;">`)
+        let nameInSpan = nameSplittedHtml[1]
+        let name = nameInSpan.split("</span>")[0]
+        let singleResult = `${name},${roll},${marks}\n`
+        fs.appendFile("result.txt",singleResult,(err)=>{
+            if(err){
+                console.log(err)
+            }
+        })
+        console.log(singleResult)
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+start = 301302219001
+end = 301302219159
+
+for(let i = start;i<=end;i++){
+    fetchResult(i)
+}
