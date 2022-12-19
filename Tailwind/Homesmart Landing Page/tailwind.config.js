9cac60633f44109fe3bb2e4dd00ec1ffb5444ddd
Kritebh Lagan Bibhakar <54534607+kritebh@users.noreply.github.com>
2022-12-19 09:34:10 +0000

    header done in homesmart

--- Tailwind/Homesmart Landing Page/tailwind.config.js
+++ Tailwind/Homesmart Landing Page/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        '16': '16rem',
+        },
+    },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 }
