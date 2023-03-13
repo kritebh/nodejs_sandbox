fb99767a9f04afe283a0bfd0011f39379191a85d
Kritebh Lagan Bibhakar <54534607+kritebh@users.noreply.github.com>
2023-03-13 17:36:46 +0000

    redux started

---
+++ React Redux/cakeshop/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import CakeContainer from './Components/CakeContainer';
+function App() {
+  return (
+    <div className='App'>
+      <CakeContainer />
+    </div>
+  );
+}
+
+export default App;
