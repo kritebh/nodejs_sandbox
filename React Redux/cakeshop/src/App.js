ccdb353a0ebd105439f73f0a4450db765d6f67d2
Kritebh Lagan Bibhakar <54534607+kritebh@users.noreply.github.com>
2023-03-19 09:16:43 +0000

    cake.js

--- React Redux/cakeshop/src/App.js
+++ React Redux/cakeshop/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import CakeContainer from './Components/CakeContainer';
+import { Provider } from 'react-redux';
+import store from './redux/cakes/store'
 function App() {
   return (
+    <Provider store = {store}>
     <div className='App'>
       <CakeContainer />
     </div>
+    </Provider>
   );
 }
 
