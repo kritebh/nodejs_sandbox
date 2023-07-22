700f7773b756572fdf61fd967d02aefee760bb9b
kritebh <kritebhlaganbibhakar@gmail.com>
2023-07-22 10:48:12 +0530

    multiple env tested

--- Dynamic env/server.js
+++ Dynamic env/server.js
@@ -1,6 +1,6 @@
 const path = require("path")
 const dotenv = require("dotenv")
-// dotenv.config({path: path.join(__dirname,`.env.${process.env.NODE_ENV.trim()}`)})
+dotenv.config({path: path.join(__dirname,`.env.${process.env.NODE_ENV.trim()}`)})
 
-console.log(path.join(__dirname,`.env.${process.env.NODE_ENV}`))
+// console.log(path.join(__dirname,`.env.${process.env.NODE_ENV}`))
 console.log(process.env.DB_URL)
