707127781b83461650724613e1f74904fc98c943
kritebh <kritebhlaganbibhakar@gmail.com>
2023-08-01 20:14:24 +0530

    writeMany.js

--- Streams/first/writeMany.js
+++ Streams/first/writeMany.js
@@ -1,13 +1,48 @@
-const fs = require('fs/promises');
+// const fs = require('fs/promises');
 
-(async ()=>{
+/*
+//using Promises
+// Time : 1:23.303 (m:ss.mmm)
+*/
+// (async ()=>{
 
-    console.time("start");
-    let file = await fs.open("test.txt",'w');
-    
-    for(let i=0;i<1000000;i++){
-        file.write(`${i} `);
+//     console.time("start");
+//     let file = await fs.open("test.txt",'w');
+
+//     for(let i=0;i<1000000;i++){
+//         file.write(`${i} `);
+//     }
+//     console.timeEnd("start");
+// }
+// )()
+
+// const fs = require('fs');
+
+/*
+//using Promises
+// Time : 0:10.570 (m:ss.mmm)
+*/
+// (async () => {
+//   console.time("start");
+//   fs.open("test.txt", "w", (err, fd) => {
+//     for (let i = 0; i < 1000000; i++) {
+//       fs.write(fd,`${i} `,()=>{})
+//     }
+//     console.timeEnd("start");
+//   });
+// })();
+
+const fs = require("fs/promises");
+
+(async () => {
+  console.time("start");
+  let file = await fs.open("test.txt", "w");
+  const stream = file.createWriteStream();
+  let buff;  
+  for (let i = 0; i < 1000000; i++) {
+    buff = Buffer.from(`${i} `,'utf-8');
+          stream.write(buff)
     }
     console.timeEnd("start")
-}
-)()
+
+})();
